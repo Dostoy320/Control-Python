4ddb205615e7094bcf9b50ae5c487fed1fef89f2
Dostoy320 <dostoy320@gmail.com>
2013-07-25 22:15:20 -0500

    comments

--- motor_1.py
+++ motor_1.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+# set pin formatting system (vs BOARD)
 GPIO.setmode(GPIO.BCM)
 
+#assign names to pins in use
 enable_pin = 18
 coil_A_1_pin = 4
 coil_A_2_pin = 17
 coil_B_1_pin = 23
 coil_B_2_pin = 24
 
+#set pins to output
 GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
@@ -45,21 +48,39 @@ def setStep(w1, w2, w3, w4):
   GPIO.output(coil_B_1_pin, w3)
   GPIO.output(coil_B_2_pin, w4)
 
+#time between coil phase changes
 delay = .003
 
-cont = "y"
 
-while (cont == "y"):
-	forsteps = raw_input("Forward rotation in steps: ")
-	backsteps = raw_input("Backward rotation in steps: ")
-	print "Confirm: %s steps forward. \n \t %s steps back." % (forsteps, backsteps)
-	confirm = raw_input("y/n >> ")
-	if (confirm == "y"):
-		print "\n processing request...\n"
-		forward(int(forsteps))
-		backwards(int(backsteps))
-	else:
-		pass	
-	cont = raw_input("New input? y/n: ")
+cont = "y"  #initial state to start while loop
 
-GPIO.cleanup()
+
+print "--------------------------------------" #divider for UI purposes
+
+try:
+
+	while (cont == "y"):
+		forsteps = raw_input("Forward rotation in steps: ")
+		backsteps = raw_input("Backward rotation in steps: ")
+		print "Confirm: %s steps forward. \n \t %s steps back." % (forsteps, backsteps)
+		confirm = raw_input("y/n >> ")
+		if (confirm == "y"):
+			print "\n processing request...\n"
+			forward(int(forsteps))
+			backwards(int(backsteps))
+		else:
+			pass
+
+		cont = raw_input("New input? y/n: ")
+
+finally:
+# IMPORTANT!!
+#
+# .cleanup un-initializes the GPIO pins.
+#
+# Without this, the motor coils stay energized
+# while the program is waiting long periods 
+# for an input, or if the program throws an error.
+# The energized coils can become surprisinly hot!
+##################################################
+	GPIO.cleanup()
