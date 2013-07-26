53c98665d95fb3d2655998e59f2a04f4ed1053c5
Dostoy320 <dostoy320@gmail.com>
2013-07-25 21:14:39 -0500

    initial working program

--- motor_1.py
+++ motor_1.py
@@ -1,3 +1,60 @@
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+
+enable_pin = 18
+coil_A_1_pin = 4
+coil_A_2_pin = 17
+coil_B_1_pin = 23
+coil_B_2_pin = 24
+
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(coil_A_1_pin, GPIO.OUT)
+GPIO.setup(coil_A_2_pin, GPIO.OUT)
+GPIO.setup(coil_B_1_pin, GPIO.OUT)
+GPIO.setup(coil_B_2_pin, GPIO.OUT)
+
+GPIO.output(enable_pin, 1)
+
+def forward(steps):  
+  for i in range(0, steps):
+    setStep(1, 0, 1, 0)
+    time.sleep(delay)
+    setStep(0, 1, 1, 0)
+    time.sleep(delay)
+    setStep(0, 1, 0, 1)
+    time.sleep(delay)
+    setStep(1, 0, 0, 1)
+    time.sleep(delay)
+
+def backwards(steps):  
+  for i in range(0, steps):
+    setStep(1, 0, 0, 1)
+    time.sleep(delay)
+    setStep(0, 1, 0, 1)
+    time.sleep(delay)
+    setStep(0, 1, 1, 0)
+    time.sleep(delay)
+    setStep(1, 0, 1, 0)
+    time.sleep(delay)
+
+def setStep(w1, w2, w3, w4):
+  GPIO.output(coil_A_1_pin, w1)
+  GPIO.output(coil_A_2_pin, w2)
+  GPIO.output(coil_B_1_pin, w3)
+  GPIO.output(coil_B_2_pin, w4)
+
+delay = .003
+
+forsteps = raw_input("Forward rotation in steps:")
+backsteps = raw_input("Backward rotation in steps:")
+print "Confirm: %s steps forward. \n \t %s steps back." % (forsteps, backsteps)
+confirm = raw_input("y/n >>")
+if (confirm == "y"):
+	forward(int(forsteps))
+	backwards(int(backsteps))
+else:
+	pass	
+
+GPIO.cleanup()
