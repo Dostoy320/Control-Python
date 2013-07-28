28183fa4ba52bc93930f83e54fe3d9b5c1b5f186
Dostoy320 <dostoy320@gmail.com>
2013-07-28 00:22:55 -0500

    Added save file functionality, which allows for returning to the initial state (0).

--- motor_1.py
+++ motor_1.py
@@ -20,58 +20,95 @@ GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
 GPIO.output(enable_pin, 1)
 
-def forward(steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
 
-def backwards(steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
+def forward(steps, position): 
+	global postion 
+	for i in range(0, steps):
+		setStep(1, 0, 1, 0)
+		time.sleep(delay)
+		setStep(0, 1, 1, 0)
+		time.sleep(delay)
+		setStep(0, 1, 0, 1)
+		time.sleep(delay)
+		setStep(1, 0, 0, 1)
+		time.sleep(delay)
+		position = position + 1
+
+	return position
+
+
+def backwards(steps, position):  
+	for i in range(0, steps):
+		setStep(1, 0, 0, 1)
+		time.sleep(delay)
+		setStep(0, 1, 0, 1)
+		time.sleep(delay)
+		setStep(0, 1, 1, 0)
+		time.sleep(delay)
+		setStep(1, 0, 1, 0)
+		time.sleep(delay)
+		position = position - 1
+
+	return position
 
 def setStep(w1, w2, w3, w4):
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+	GPIO.output(coil_A_1_pin, w1)
+	GPIO.output(coil_A_2_pin, w2)
+	GPIO.output(coil_B_1_pin, w3)
+	GPIO.output(coil_B_2_pin, w4)
 
-#time between coil phase changes
+
+def savefile(position):
+	w = open("position.sav", "w")
+	w.write(str(position))
+	w.close()
+
+
+#rotation speed (time between coil phase changes) smaller number faster.  max = .002
 delay = .003
-
 
 cont = "y"  #initial state to start while loop
 
 
-print "--------------------------------------" #divider for UI purposes
+print "--------------------------------------" #divider for UI purposesposition = 0
+
+
 
 try:
+	
+	f = open("position.sav")
+	r = int(f.read())
+	position = r
 
 	while (cont == "y"):
-		forsteps = raw_input("Forward rotation in steps: ")
-		backsteps = raw_input("Backward rotation in steps: ")
+		forsteps = int(raw_input("Forward rotation in steps: "))
+		backsteps = int(raw_input("Backward rotation in steps: "))
 		print "Confirm: %s steps forward. \n \t %s steps back." % (forsteps, backsteps)
 		confirm = raw_input("y/n >> ")
 		if (confirm == "y"):
 			print "\n processing request...\n"
-			forward(int(forsteps))
-			backwards(int(backsteps))
+			position = forward(forsteps, position)
+			position = backwards(backsteps, position)
+			print position
 		else:
 			pass
 
 		cont = raw_input("New input? y/n: ")
+		if (cont == "n"):
+			zero = raw_input("back to zero? y/n: ")
+			print position
+			if (zero == "y"):
+				if (position > 0):
+					backsteps = position
+					position = backwards(backsteps, position)
+					savefile(position)
+				else:
+					forsteps = -position
+					position = forward(forsteps, position)
+					savefile(position)
+			else:
+				savefile(position)
+		
 
 finally:
 # IMPORTANT!!
